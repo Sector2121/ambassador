c3726a0e575508739678ed73f1f261743f199dfa
Földvári Ádám <foldvariadamdavid@gmail.com>
2023-10-07 19:07:44 +0200

    Android button navigation back at password page work correctly now

--- lib/pages/password/view/password_page.dart
+++ lib/pages/password/view/password_page.dart
@@ -18,93 +18,102 @@ class _PasswordPageState extends State<PasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => GetIt.instance.get<PasswordBloc>(),
-      child: BlocConsumer<PasswordBloc, PasswordState>(
-        listener: (context, state) async {
-          if (state is PasswordLoadedState) {
-            NavigationService.of(context).goToLeaderboardPage();
-          } else if (state is PasswordWrongState) {
-            _messageService.showMessage(context, 'Wrong Password', state.message);
-          }
-        },
-        builder: (context, state) {
-          if (state is PasswordLoadingState) {
-            return const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return WillPopScope(
+      onWillPop: _navigateBack,
+      child: BlocProvider(
+        create: (context) => GetIt.instance.get<PasswordBloc>(),
+        child: BlocConsumer<PasswordBloc, PasswordState>(
+          listener: (context, state) async {
+            if (state is PasswordLoadedState) {
+              NavigationService.of(context).goToLeaderboardPage();
+            } else if (state is PasswordWrongState) {
+              _messageService.showMessage(context, 'Wrong Password', state.message);
+            }
+          },
+          builder: (context, state) {
+            if (state is PasswordLoadingState) {
+              return const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: CircularProgressIndicator(),
+                  ),
+                ],
+              );
+            } else if (state is PasswordLoadedState) {
+              return Container();
+            }
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: CircularProgressIndicator(),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _navigateBack();
+                      },
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                    const Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Sign in',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text('You have an account. Please complete the fields below'),
+                const SizedBox(
+                  height: 20,
+                ),
+                CustomTextField(
+                  controller: _passwordController,
+                  textChangedCallBack: setButtonIfNecessary,
+                  obscure: true,
+                  icon: const Icon(Icons.key),
+                  suffixIconVisible: true,
+                  labelTetx: 'Password',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomButton(
+                        buttonDisabled: buttonDisabled,
+                        callback: () =>
+                            context.read<PasswordBloc>().add(PasswordSentEvent(password: _passwordController.text)),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             );
-          } else if (state is PasswordLoadedState) {
-            return Container();
-          }
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      context.read<EmailBloc>().add(Reset());
-                      NavigationService.of(context).goBack();
-                    },
-                    child: const Icon(Icons.arrow_back),
-                  ),
-                  const Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Sign in',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                        ),
-                        SizedBox(
-                          width: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text('You have an account. Please complete the fields below'),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomTextField(
-                controller: _passwordController,
-                textChangedCallBack: setButtonIfNecessary,
-                obscure: true,
-                icon: const Icon(Icons.key),
-                suffixIconVisible: true,
-                labelTetx: 'Password',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomButton(
-                      buttonDisabled: buttonDisabled,
-                      callback: () =>
-                          context.read<PasswordBloc>().add(PasswordSentEvent(password: _passwordController.text)),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          );
-        },
+          },
+        ),
       ),
     );
+  }
+
+  Future<bool> _navigateBack() async {
+    context.read<EmailBloc>().add(Reset());
+    NavigationService.of(context).goBack();
+
+    return false;
   }
 
   void setButtonIfNecessary() {
